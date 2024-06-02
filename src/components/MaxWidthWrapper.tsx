09254f8dc78b1bad902aee82f41b9b7e62c48c8e
hemanth <hemanth02135@gmail.com>
2024-06-02 20:34:11 +0530

    landing page/initial commit

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+import React, { ReactNode } from 'react';
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({ className, children }) => {
+  return (
+    <div className={cn('h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
